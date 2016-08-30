d8dd1c9e5486ec89e421dfa2a83a1e906267f2d8
Don Kingery <Don Kingery>
2016-08-30 11:25:49 -0400

    1st Subtract Test

--- 3rdClassTest.rb
+++ 3rdClassTest.rb
@@ -1,22 +1,35 @@
 require "minitest/autorun"
 require_relative "3rdClassFunc.rb"
+require_relative "3rdClass_Subtract_Func.rb"
 
-class TestadditionFunction < Minitest::Test
+class TestAdditionFunction < Minitest::Test
+		def test_1_equals_1
+		assert_equal(1,1)
+		end
 
-def test_1_equals_1
-	assert_equal(1,1)
-	
-	end
-
-	def test_1_plus_1
+		def test_1_plus_1
 		assert_equal(2, add(1,1))
-	end
+		end
 
-def test_0_plus_1
+		def test_0_plus_1
 		assert_equal(1, add(0,1))
-	end
+		end
 
 		def test_5_plus_neg3
 		assert_equal(2, add(5,-3))
+		end
+end
+
+
+class TestsubtractionFunction < Minitest::Test
+#	def test_1_equals_1
+#	assert_equal(1,1)
+	
+#	end
+
+	def test_10_minus_5
+		assert_equal(5, sub(10,5))
 	end
+
+
 end
