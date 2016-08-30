686c69e5b7b3b4b279344c3191e2af5122fc5966
Don Kingery <Don Kingery>
2016-08-30 11:03:02 -0400

    3rd class, 4th test

--- 3rdClassTest.rb
+++ 3rdClassTest.rb
@@ -12,8 +12,11 @@ def test_1_equals_1
 		assert_equal(2, add(1,1))
 	end
 
-def test_1_plus_1
+def test_0_plus_1
 		assert_equal(1, add(0,1))
-	
+	end
+
+		def test_5_plus_neg3
+		assert_equal(2, add(5,-3))
 	end
 end
